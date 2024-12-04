5e07b3407e25ba67727b4cb53a0af4e5be99ad80
Gabriel Valente <valentefluminense@gmail.com>
2024-12-04 20:26:01 -0300

    modularizacao da integracao da interface com a camera

--- Gabriel/envia_serial.py
+++ Gabriel/envia_serial.py
@@ -1,10 +1,11 @@
 from serial import Serial
 
-__all__ = ['enviar_serial', 'enviar_batida', 'enviar_hue', 'enviar_brightness']
+__all__ = ['enviar_serial', 'enviar_batida', 'enviar_hue', 'enviar_brightness', 'envia_pose']
 
-arduino_serial = Serial("COM7", baudrate=9600)
+# arduino_serial = Serial("COM7", baudrate=9600)
 
 def enviar_serial(texto):
+    return # só pra testar sem o arduino
     arduino_serial.write(texto.encode("UTF-8"))
     # texto_recebido = arduino_serial.readline().decode().strip()
 
@@ -19,3 +20,17 @@ def enviar_hue(hue):
 def enviar_brightness(brightness):
     enviar_serial("brightness %d\n"%brightness)
     print("brightness ", brightness)
+
+def envia_pose(string):
+    ''' 
+    Função que envia a pose (string) para o Arduino.
+
+    É necessário colocar 'Movimento ' antes de enviar o nome da pose,
+    pois a função no Arduino foi definida de forma que precisa receber 
+    um identificador de movimento para saber que se trata de um movimento.
+    '''
+    enviar_serial(f"{'Movimento ' + string}\n")
+    print(f"{'Movimento ' + string}\n")
+    return 
+
+#OBS: Vamos ter que decidir como diferenciar movimentos de braços e pernas no Arduino, e alterar esta função depois
